c2b4d8818fede32913e5c5449f420ac719ccc147
NovaFlicker <161892101+AbidAbdullah199@users.noreply.github.com>
2024-07-25 14:35:57 +0600

    you can put variables here

---
+++ config.py
@@ -0,0 +1,72 @@
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # fill up ids
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+    # database
+    DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+    DB_URL  = os.environ.get("DB_URL","")
+ 
+    # extra
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    PORT = int(os.environ.get("PORT", ""))
+    
+     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+
+
+class Txt(object):
+        
+    START_TXT = """Hᴇʟʟᴏ {} 
+I ᴀᴍ Nɪᴄᴏ Rᴏʙɪɴ
+I Cᴀɴ Rᴇɴᴀᴍᴇ Fɪʟᴇ Wɪᴛʜ Pᴀʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Mᴀᴋᴇ Oᴜᴛᴘᴜᴛ Wɪᴛʜ Yᴏᴜʀ Dᴇsɪʀᴇᴅ Fᴏʀᴍᴀᴛ Tʀʏ Tᴏ Usᴇ Mᴇ Oʀ Sᴇɴᴅ /ʜᴇʟᴘ Fᴏʀ Mᴏʀᴇ Assɪsᴛᴇɴᴛs.
+"""
+    
+    FILE_NAME_TXT = """<b><u>robinrenamebot</u></b>
+
+    
+    ABOUT_TXT = f"""
+<b>○ Cʜᴀɴɴᴀʟ :</b> <a href='https://t.me/AnimeQuestX'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a>
+<b>○ Hɪɴᴅɪ Cʜᴀɴɴᴀʟ :</b> <a href='https://t.me/AnimeQuestHindi'>Jᴏɪɴ Nᴏᴡ</a>
+<b>○ Oɴɢᴏɪɴɢ Cʜᴀɴɴᴀʟ :</b> <a href='https://t.me/OngoingAnimeQuest'>Jᴏɪɴ Nᴏᴡ</a>
+<b>○ Dɪsᴄᴜssᴛɪᴏɴ Gʀᴏᴜᴘ :</b> <a href='https://t.me/+r-x-wA4JT5gxZjVl'>Jᴏɪɴ Nᴏᴡ</a>
+<b>○ Oᴡɴᴇʀ :</b> <a href='https://t.me/abidabdullah199'>Mᴏɴᴋᴇʏ D Lᴜғғʏ</a>
+"""
+
+    
+    THUMBNAIL_TXT = """<b><u>  Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴᴀɪʟ</u></b>
+    
+⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....
+    
+⦿ /viewthumb - Use This Command To See Your Thumbnail
+⦿ /delthumb - Use This Command To Delete Your Thumbnail"""
+
+    CAPTION_TXT = """<b><u>  Hᴏᴡ Tᴏ Sᴇᴛ Cᴀᴘᴛɪᴏɴ</u></b>
+    
+⦿ /set_caption - Use This Command To Set Your Caption
+⦿ /see_caption - Use This Command To See Your Caption
+⦿ /del_caption - Use This Command To Delete Your Caption"""
+
+    PROGRESS_BAR = """<b>\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣ ⏳️ Dᴏɴᴇ : {0}%
+┣ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣ ⏰️ Eᴛᴀ: {4}
+┣ 🥺 Join: @AnimeQuestX
+╰━━━━━━━━━━━━━━━ </b>"""
+    
+    
+    DONATE_TXT = """<b>Dᴏɴᴀᴛᴇ Tʜᴏᴏsᴇ Wʜᴏ Rᴇᴀʟʟʏ Nᴇᴇᴅ Iᴛ ❤️</b>
+    
+ """
+    
+    HELP_TXT = """Iɴʙᴏx Mᴇ @abidabdullah199 """
+
